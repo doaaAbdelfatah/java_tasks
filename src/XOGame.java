07fb194c9ac194f414187b3853366f3e9062f182
Doaa Abd Elfatah <doaa.abdelftah@gmail.com>
2024-04-04 13:04:29 +0200

    session 104

--- src/XOGame.java
+++ src/XOGame.java
@@ -1,21 +1,74 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class XOGame {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         char [][] xo = new char[3][3];
+
+        // fill Array by space char
         for (int i = 0; i < 3; i++) {
             Arrays.fill(xo[i] , ' ');
         }
+        //////////////////////////
 
-        xo[0][1] = 'X';
-        xo[1][1] = 'O';
-        xo[0][0] = 'X';
-        for (char [] row : xo){
-            System.out.print("| ");
-            for (char c : row){
+        char player = 'X' ;
+        do{
+            printGame(xo);
+
+            System.out.println("please Enter " + player + " location (row ,column):");
+            System.out.print("Row : ");
+            int r = scanner.nextInt(); // 0
+            System.out.print("Column : ");
+            int c = scanner.nextInt(); // 0
+            xo[r][c] = player;
+
+            // check winner // call method
+            boolean isWin = checkWinner(xo , player); // true , false
+
+            if (!isWin){
+                // if false
+                if (player =='X') player ='O'; else player ='X';
+            }else{
+                // if true
+                // draw game
+                printGame(xo);
+                System.out.println("Congratulation " + player + " Win");
+                break;
+            }
+        }while(true);
+    }
+
+    // declare methodd
+    static  boolean checkWinner(char [][] xo , char player){
+        if(xo[0][0] == player && xo[0][1] == player && xo[0][2] ==player) return  true;
+        else if(xo[1][0] == player && xo[1][1] == player && xo[1][2] ==player) return  true;
+        else if(xo[2][0] == player && xo[2][1] == player && xo[2][2] ==player) return  true;
+        else if(xo[0][0] == player && xo[1][0] == player && xo[2][0] ==player) return  true;
+        else if(xo[0][1] == player && xo[1][1] == player && xo[2][1] ==player) return  true;
+        else if(xo[0][2] == player && xo[1][2] == player && xo[2][2] ==player) return  true;
+        else if(xo[0][0] == player && xo[1][1] == player && xo[2][2] ==player) return  true;
+        else if(xo[0][2] == player && xo[1][1] == player && xo[2][0] ==player) return  true;
+        return  false ;
+    }
+    static  void printGame(char [][]xo ){
+        // print game (xo Array)
+        int rowCount =0;
+        System.out.println("    0   1   2  ");
+        for (char [] row : xo){ // get row from the Array
+            System.out.print(rowCount++ +  " | ");
+            for (char c : row){  // get element value from the row
                 System.out.print( c + " | ");
             }
             System.out.println();
         }
+        ///////////////////////
 
     }
+
 }
+
+
+
